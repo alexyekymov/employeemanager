0fe63c1e7e7dc1f653202eb313d0430e914b516b
alexyekymov <alexyekymov@gmail.com>
2021-02-22 21:04:25 +0200

    REST crud impl

---
+++ src/main/java/com/alexyekymov/employeemanager/repositoty/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.alexyekymov.employeemanager.repositoty;
+
+import com.alexyekymov.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository  extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
